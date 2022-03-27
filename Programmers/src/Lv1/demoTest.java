6e1734bea988f953abbc363bdaa32bfe73fbdaa8
lim950808 <lim080895@gmail.com>
2022-03-27 23:41:40 +0900

    모의고사

---
+++ Programmers/src/Lv1/demoTest.java
@@ -0,0 +1,30 @@
+package Lv1;
+
+import java.util.ArrayList;
+//모의고사
+class demoTest {
+  
+  public int[] solution(int[] answer) {
+      
+      int[] a = {1, 2, 3, 4, 5};
+      int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+      int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+      int[] score = new int[3];
+      
+      for(int i=0; i<answer.length; i++) {
+          if(answer[i] == a[i%a.length]) {score[0]++;}
+          if(answer[i] == b[i%b.length]) {score[1]++;}
+          if(answer[i] == c[i%c.length]) {score[2]++;}
+      }
+      
+      int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+      ArrayList<Integer> list = new ArrayList<>();
+      if(maxScore == score[0]) {list.add(1);}
+      if(maxScore == score[1]) {list.add(2);}
+      if(maxScore == score[2]) {list.add(3);}
+      
+      return list.stream().mapToInt(i->i.intValue()).toArray();
+      //가장 많은 문제를 맞힌 사람
+  }
+  
+}
