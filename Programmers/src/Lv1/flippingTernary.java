3c5b6c98c2df5eb2b7e130d3724fb2717cb08531
lim950808 <lim080895@gmail.com>
2022-04-09 13:39:32 +0900

    3진법 뒤집기

---
+++ Programmers/src/Lv1/flippingTernary.java
@@ -0,0 +1,23 @@
+package Lv1;
+
+import java.util.*;
+//3진법 뒤집기
+class flippingTernary {
+    public int solution(int n) { //매개변수 n
+        int answer = 0;
+        ArrayList<Integer> temp = new ArrayList<>();
+        
+        // 10진법 -> 3진법(역순)
+        while(true){
+            if(n<3){ temp.add(n); break; }
+            temp.add(n%3);
+            n = n/3;
+        }
+        // 3진법(역순) -> 10진법
+        for(int i=0; i<temp.size(); i++){
+            answer += (Math.pow(3,temp.size()-i-1)*temp.get(i));
+        }
+        
+        return answer;
+    }
+}
