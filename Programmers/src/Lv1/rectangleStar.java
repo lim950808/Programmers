56cbf147f863c63cc30c2761bceda0d100b97f5f
lim950808 <lim080895@gmail.com>
2022-04-14 00:01:10 +0900

    직사각형 별찍기

---
+++ Programmers/src/Lv1/rectangleStar.java
@@ -0,0 +1,18 @@
+package Lv1;
+
+import java.util.Scanner;
+//직사각형 별찍기
+public class rectangleStar {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(); //열
+		int b = sc.nextInt(); //행
+
+		for(int i=0; i<b; i++) { //행
+			for(int j=0; j<a; j++) { //열
+				System.out.print("*"); 
+			}
+			System.out.println();
+		}
+	}
+}
