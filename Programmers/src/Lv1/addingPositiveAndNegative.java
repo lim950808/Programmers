4181d6ef683eedad4a60800b2b68585fd4461c47
lim950808 <lim080895@gmail.com>
2022-04-22 23:35:15 +0900

    음양 더하기

---
+++ Programmers/src/Lv1/addingPositiveAndNegative.java
@@ -0,0 +1,17 @@
+package Lv1;
+//음양 더하기
+class addingPositiveAndNegative {
+  public int solution(int[] absolutes, boolean[] signs) {
+      int answer = 0;
+      
+      for(int i=0; i<signs.length; i++) {
+          if(signs[i]) {
+              answer += absolutes[i];
+          }else {
+              answer -= absolutes[i];
+          }
+      }
+      
+      return answer;
+  }
+}
