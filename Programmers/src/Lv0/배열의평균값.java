cfe07894b5c5509d9e1b1d35d0ddaea96c113207
jaeeoklim <lim080895@gmail.com>
2022-11-15 23:59:54 +0900

    배열의 평균값

---
+++ Programmers/src/Lv0/배열의평균값.java
@@ -0,0 +1,17 @@
+package Lv0;
+
+class 배열의평균값 {
+    public double solution(int[] arr) {
+
+        double answer = 0;
+        int sum = 0;
+
+        for (int i=0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        answer = (double)sum/arr.length;
+
+        return answer;
+    }
+}
