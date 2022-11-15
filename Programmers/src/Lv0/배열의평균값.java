b8072699f8ba0fb088e23dd21c17b8eb5630e897
jaeeoklim <lim080895@gmail.com>
2022-11-16 00:14:15 +0900

    배열의 평균값(수정), 피자나눠먹기1

--- Programmers/src/Lv0/배열의평균값.java
+++ Programmers/src/Lv0/배열의평균값.java
@@ -2,7 +2,6 @@ package Lv0;
 
 class 배열의평균값 {
     public double solution(int[] arr) {
-
         double answer = 0;
         int sum = 0;
 
@@ -11,7 +10,17 @@ class 배열의평균값 {
         }
 
         answer = (double)sum/arr.length;
-
         return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr2 = new int[]{89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
+
+        double answer1 = new 배열의평균값().solution(arr1);
+        double answer2 = new 배열의평균값().solution(arr2);
+
+        System.out.println(answer1); // 5.5
+        System.out.println(answer2); // 94.0
     }
 }
