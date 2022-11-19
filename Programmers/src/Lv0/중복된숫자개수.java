2b1517643da41616b0de0ba2616ade855e348ea4
jaeeoklim <lim080895@gmail.com>
2022-11-19 22:31:44 +0900

    중복된 숫자 개수 (테스트 코드 추가)

--- Programmers/src/Lv0/중복된숫자개수.java
+++ Programmers/src/Lv0/중복된숫자개수.java
@@ -1,5 +1,7 @@
 package Lv0;
 
+import java.util.Arrays;
+
 class 중복된숫자개수 {
     public int solution(int[] array, int n) {
         int answer = 0;
@@ -11,5 +13,16 @@ class 중복된숫자개수 {
 
         // array에 n이 몇 개 있는 지를 return
         return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{1, 1, 2, 3, 4, 5};
+        int[] arr2 = new int[]{0, 2, 3, 4};
+
+        int answer1 = new 중복된숫자개수().solution(arr1, 1);
+        int answer2 = new 중복된숫자개수().solution(arr2, 1);
+
+        System.out.println(Arrays.toString(arr1) + "에는 1이 " + answer1 + "개 있습니다."); // 2
+        System.out.println(Arrays.toString(arr2) + "에는 1이 " + answer2 + "개 있습니다."); // 0
     }
 }
