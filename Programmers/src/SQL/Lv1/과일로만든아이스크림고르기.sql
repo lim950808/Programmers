21bc5fe44bdaf56e8465ab1dc41196dc1b7d5553
Jae Eok Lim <lim080895@gmail.com>
2023-02-27 10:49:49 +0900

    Programmers SQL Lv1 (Oracle)

---
+++ Programmers/src/SQL/Lv1/과일로만든아이스크림고르기.sql
@@ -0,0 +1,16 @@
+-- 상반기 아이스크림 총주문량이 3,000보다 높으면서 아이스크림의 주 성분이 과일인 아이스크림의 맛을 총주문량이 큰 순서대로 조회하는 SQL 문을 작성해주세요.
+-- Oracle
+-- SELECT
+
+SELECT
+    F.FLAVOR
+FROM
+    FIRST_HALF F,
+    ICECREAM_INFO I
+WHERE
+        F.FLAVOR = I.FLAVOR
+    AND F.TOTAL_ORDER > 3000
+    AND I.INGREDIENT_TYPE = 'fruit_based'
+ORDER BY
+    TOTAL_ORDER DESC
+;
