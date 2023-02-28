c70f3f6419137f14b3c4fb150e0a669598faa705
Jae Eok Lim <lim080895@gmail.com>
2023-02-28 23:58:27 +0900

    Programmers SQL Lv2 (Oracle)

---
+++ Programmers/src/SQL/Lv2/동물수구하기.sql
@@ -0,0 +1,7 @@
+-- 동물 보호소에 들어온 동물은 4마리입니다. 따라서 SQL문을 실행하면 다음과 같이 나와야 합니다.
+-- Oracle
+-- SUM, MAX, MIN
+
+SELECT COUNT(ANIMAL_ID) AS count
+FROM ANIMAL_INS
+;
---
+++ Programmers/src/SQL/Lv2/최솟값구하기.sql
@@ -0,0 +1,7 @@
+-- 동물 보호소에 가장 먼저 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+-- Oracle
+-- SUM, MAX, MIN
+
+SELECT MIN(DATETIME) AS "시간"
+FROM ANIMAL_INS
+;
