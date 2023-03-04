a3886b4b89646b90c55186a543617d7ff2093ab9
Jae Eok Lim <lim080895@gmail.com>
2023-03-04 23:28:52 +0900

    Programmers SQL Lv4 (Oracle)

---
+++ Programmers/src/SQL/Lv4/5월식품들의총매출조회하기.sql
@@ -0,0 +1,16 @@
+-- FOOD_PRODUCT와 FOOD_ORDER 테이블에서 생산일자가 2022년 5월인 식품들의 식품 ID, 식품 이름, 총매출을 조회하는 SQL문을 작성해주세요. 이때 결과는 총매출을 기준으로 내림차순 정렬해주시고 총매출이 같다면 식품 ID를 기준으로 오름차순 정렬해주세요.
+-- Oracle
+-- JOIN
+
+SELECT P.PRODUCT_ID, P.PRODUCT_NAME, (O.A * P.PRICE) AS TOTAL_SALES
+FROM
+(
+    SELECT PRODUCT_ID, SUM(AMOUNT) AS A
+    FROM FOOD_ORDER
+    WHERE TO_CHAR(PRODUCE_DATE, 'yyyy-mm') = '2022-05'
+    GROUP BY PRODUCT_ID
+) O
+INNER JOIN FOOD_PRODUCT P
+ON O.PRODUCT_ID = P.PRODUCT_ID
+ORDER BY TOTAL_SALES DESC, P.PRODUCT_ID
+;
