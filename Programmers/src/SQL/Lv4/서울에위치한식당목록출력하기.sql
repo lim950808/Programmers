8a125c23e2442969f5bae4e1ec6c898323bffb48
Jae Eok Lim <lim080895@gmail.com>
2023-03-05 23:50:01 +0900

    Programmers SQL Lv4 (Oracle)

---
+++ Programmers/src/SQL/Lv4/서울에위치한식당목록출력하기.sql
@@ -0,0 +1,11 @@
+-- REST_INFO와 REST_REVIEW 테이블에서 서울에 위치한 식당들의 식당 ID, 식당 이름, 음식 종류, 즐겨찾기수, 주소, 리뷰 평균 점수를 조회하는 SQL문을 작성해주세요. 이때 리뷰 평균점수는 소수점 세 번째 자리에서 반올림 해주시고 결과는 평균점수를 기준으로 내림차순 정렬해주시고, 평균점수가 같다면 즐겨찾기수를 기준으로 내림차순 정렬해주세요.
+-- Oracle
+-- SELECT
+
+SELECT A.REST_ID, A.REST_NAME, A.FOOD_TYPE, A.FAVORITES, A.ADDRESS, ROUND(Avg(B.REVIEW_SCORE), 2) AS SCORE
+FROM REST_INFO A
+         INNER JOIN REST_REVIEW B ON A.REST_ID = B.REST_ID
+WHERE A.ADDRESS LIKE '서울%'
+GROUP BY A.REST_ID, A.REST_NAME, A.FOOD_TYPE, A.FAVORITES, A.ADDRESS
+ORDER BY SCORE DESC, A.FAVORITES DESC
+;
---
+++ Programmers/src/SQL/Lv4/식품분류별가장비싼식품의정보조회하기.sql
@@ -0,0 +1,15 @@
+-- FOOD_PRODUCT 테이블에서 식품분류별로 가격이 제일 비싼 식품의 분류, 가격, 이름을 조회하는 SQL문을 작성해주세요. 이때 식품분류가 '과자', '국', '김치', '식용유'인 경우만 출력시켜 주시고 결과는 식품 가격을 기준으로 내림차순 정렬해주세요.
+-- Oracle
+-- GROUP BY
+
+SELECT CATEGORY, PRICE, PRODUCT_NAME
+FROM FOOD_PRODUCT
+WHERE (CATEGORY, PRICE) IN
+      (
+          SELECT CATEGORY, max(PRICE) AS MAX_PRICE
+          FROM FOOD_PRODUCT
+          WHERE CATEGORY IN ('과자', '국', '김치', '식용유')
+          GROUP BY CATEGORY
+      )
+ORDER BY PRICE DESC
+;
